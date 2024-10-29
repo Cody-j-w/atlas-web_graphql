f0e10b7c2cbda4e11aef8b769e2b9f71508a99aa
Cody Walenciak <codywalenciak@gmail.com>
2024-10-29 10:57:41 -0500

    Add mongoose connection

--- GraphQL_API/server/app.js
+++ GraphQL_API/server/app.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const {graphqlHTTP} = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+const uri = process.env.CONNECTION;
+
+mongoose.connect(uri);
+mongoose.connection.once('open', () => {
+  console.log('connected to database');
+  });
 
 const app = express();
 
