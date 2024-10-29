ae678026f01a6f968d348262dfb7a854b8870596
Cody Walenciak <codywalenciak@gmail.com>
2024-10-29 10:57:52 -0500

    Add schemas

---
+++ GraphQL_API/server/models/task.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const model = mongoose.model;
+
+const taskSchema = new Schema({
+    title: String,
+    weight: Number,
+    description: String
+});
+
+const Task = model('Task', taskSchema);
+module.exports = Task;
